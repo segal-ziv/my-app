a8d567a75c6acced35429bd1a2d3aaece827103f
segal-ziv <z.segal.pro@gmail.com>
2024-10-08 22:26:24 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAqRC9_eGOGDDzxjtWa3erg_A5r_OruVcU",
-  authDomain: "happylist-15e5c.firebaseapp.com",
-  projectId: "happylist-15e5c",
-  storageBucket: "happylist-15e5c.appspot.com",
-  messagingSenderId: "782191325879",
-  appId: "1:782191325879:web:693263a6df08662e9cd332",
-  measurementId: "G-72W044VJCP"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAqRC9_eGOGDDzxjtWa3erg_A5r_OruVcU",
+    authDomain: "happylist-15e5c.firebaseapp.com",
+    projectId: "happylist-15e5c",
+    storageBucket: "happylist-15e5c.appspot.com",
+    messagingSenderId: "782191325879",
+    appId: "1:782191325879:web:693263a6df08662e9cd332",
+    measurementId: "G-72W044VJCP"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
